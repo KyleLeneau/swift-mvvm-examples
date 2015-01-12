eca5951ad8eb2ab306ef54898f6ee3d26b018664
Kyle LeNeau <k.leneau@gmail.com>
2015-01-11 21:01:15 -0600

    Initial commit with project and dependencies setup

---
+++ swift-mvvm-examples/Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Bridging-Header.h
+//  swift-mvvm-examples
+//
+//  Created by Kyle LeNeau on 1/11/15.
+//  Copyright (c) 2015 Kyle LeNeau. All rights reserved.
+//
+
+#import "PureLayout.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
---
+++ swift-mvvm-examplesTests/swift_mvvm_examplesTests.swift
@@ -0,0 +1,36 @@
+//
+//  swift_mvvm_examplesTests.swift
+//  swift-mvvm-examplesTests
+//
+//  Created by Kyle LeNeau on 1/11/15.
+//  Copyright (c) 2015 Kyle LeNeau. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class swift_mvvm_examplesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
