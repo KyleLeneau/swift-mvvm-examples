41bf76d6b7352fa2305cdda9ce2c26d253269b27
Kyle LeNeau <k.leneau@gmail.com>
2015-05-17 14:43:39 -0500

    Adding a login view model

---
+++ swift-mvvm-examples/ViewController/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  swift-mvvm-examples
+//
+//  Created by Kyle LeNeau on 5/16/15.
+//  Copyright (c) 2015 Kyle LeNeau. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ swift-mvvm-examples/ViewModel/LoginViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  LoginViewModel.swift
+//  swift-mvvm-examples
+//
+//  Created by Kyle LeNeau on 5/16/15.
+//  Copyright (c) 2015 Kyle LeNeau. All rights reserved.
+//
+
+import Foundation
+import ReactiveCocoa
+
+public class User {}
+
+public class LoginViewModel {
+    public let userName = MutableProperty<String?>(nil)
+    public let password = MutableProperty<String?>(nil)
+    
+    public lazy var loginEnabled: PropertyOf<Bool> = {
+        let property = MutableProperty(false)
+        
+        property <~ combineLatest(self.userName.producer,
+                                  self.password.producer)
+            |> map { userName, password in
+                if let userName = userName,
+                    password = password {
+                        return true
+                } else {
+                    return false
+                }
+        }
+        
+        return PropertyOf(property)
+    }()
+    
+    public lazy var loginAction: Action<(String, String), User, NSError> = {
+        return Action(enabledIf: self.loginEnabled, { username, password in
+            return self.login(username, password: password)
+        })
+    }()
+
+    private func login(username: String, password: String)
+        -> SignalProducer<User, NSError> {
+            
+        return SignalProducer { sink, disposable in
+            sendCompleted(sink)
+        }
+    }
+}
